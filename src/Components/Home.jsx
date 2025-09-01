3003c8e3e77e92d1771f9d37d827ffb55e1905a5
Rohit singh <rohitdev124421@gmail.com>
2025-09-01 14:10:33 +0530

    day 6

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,42 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 
 export default function Home() {
+  const [text, setText] = useState("");
+  const [todo, setTodo] = useState([]);
+
+  function textchange(e) {
+    setText(e.target.value);
+  }
+
+  function addclick() {
+    if (text.trim() === "") return; // prevent empty todos
+    setTodo([...todo, text]); // storing string
+    setText(""); // reset input
+  }
+
+
   return (
-    <div className="bg-red-300 mt-24">
-      This is a home page
+    <div className="text-center border-2 border-b-lime-400 bg-white text-black">
+      <h1 className="text-4xl">todo app</h1>
+      <div>
+        <input
+          value={text}
+          onChange={textchange}
+          className="bg-black text-white"
+          type="text"
+        />
+        <button onClick={addclick} className="py-2 px-2 bg-amber-400">
+          add
+        </button>
+      </div>
+
+      <ul>
+        {todo.map((tod, index) => (
+          <li key={index}>{tod}</li>  
+        ))}
+      </ul>
     </div>
   );
 }
--- src/app/layout.js
+++ src/app/layout.js
@@ -19,7 +19,6 @@ export default function RootLayout({ children }) {
              {children}
          {/* </ProductProvider> */}
  
-        <Footer />
       </body>
     </html>
   );
