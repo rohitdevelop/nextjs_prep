48e3da8478dc48cca156310108a565670521eb09
Rohit singh <rohitdev124421@gmail.com>
2025-09-10 16:23:12 +0530

    day 14

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -1,17 +1,18 @@
-import { NextResponse } from "next/server";
-import { connectDB } from "@/lib/db";
-import Product from "@/models/Product";
+// import { NextResponse } from "next/server";
+// import { connectDB } from "@/lib/db";
+// import Product from "@/models/Product";
 
-export async function GET() {
-  await connectDB();
-  const products = await Product.find();
-  return NextResponse.json(products);
-}
+// export async function GET() {
+//   await connectDB();
+//   const products = await Product.find();
+//   return NextResponse.json(products);
+// }
 
-export async function POST(req) {
-  await connectDB();
-  const body = await req.json();
-  const newProduct = new Product(body);
-  await newProduct.save();
-  return NextResponse.json(newProduct, { status: 201 });
-}
+// export async function POST(req) {
+//   await connectDB();
+//   const body = await req.json();
+//   const newProduct = new Product(body);
+//   await newProduct.save();
+//   return NextResponse.json(newProduct, { status: 201 });
+// }
+// // 
--- src/app/api/upload/route.js
+++ src/app/api/upload/route.js
@@ -1,26 +1,26 @@
-import { NextResponse } from "next/server";
-import cloudinary from "@/lib/cloudinary";
+// import { NextResponse } from "next/server";
+// import cloudinary from "@/lib/cloudinary";
 
-export async function POST(req) {
-  try {
-    const data = await req.formData();
-    const file = data.get("file"); // image from frontend
+// export async function POST(req) {
+//   try {
+//     const data = await req.formData();
+//     const file = data.get("file"); // image from frontend
 
-    const bytes = await file.arrayBuffer();
-    const buffer = Buffer.from(bytes);
+//     const bytes = await file.arrayBuffer();
+//     const buffer = Buffer.from(bytes);
 
-    // Upload to Cloudinary
-    const uploadResult = await new Promise((resolve, reject) => {
-      cloudinary.uploader
-        .upload_stream({ folder: "products" }, (error, result) => {
-          if (error) reject(error);
-          resolve(result);
-        })
-        .end(buffer);
-    });
+//     // Upload to Cloudinary
+//     const uploadResult = await new Promise((resolve, reject) => {
+//       cloudinary.uploader
+//         .upload_stream({ folder: "products" }, (error, result) => {
+//           if (error) reject(error);
+//           resolve(result);
+//         })
+//         .end(buffer);
+//     });
 
-    return NextResponse.json({ url: uploadResult.secure_url });
-  } catch (err) {
-    return NextResponse.json({ error: err.message }, { status: 500 });
-  }
-}
+//     return NextResponse.json({ url: uploadResult.secure_url });
+//   } catch (err) {
+//     return NextResponse.json({ error: err.message }, { status: 500 });
+//   }
+// }
